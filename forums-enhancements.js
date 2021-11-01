076ad535b5654902f5dbeade89372476237f4ac4
James Nylen <jnylen@gmail.com>
2021-11-01 00:28:18 +0000

    First working modification

--- forums-enhancements.js
+++ forums-enhancements.js
@@ -1,22 +1,63 @@
-window.cpForumsEnhancments = {
-	init: function() {
-		setInterval(this.interval, 750);
-	},
+window.cpForumsEnhancments = (function() {
+	var toReturn = {
+		init: function() {
+			console.log('init', this);
 
-	interval: function() {
-		if (this.currentPath !== document.location.pathname) {
-			console.log('path change', document.location.pathname);
-			this.currentPath = document.location.pathname;
+			setInterval(this.interval, 750);
+		},
+
+		interval: function() {
+			console.log('interval', this);
+
+			if (this.currentPath !== document.location.pathname) {
+				console.log('path change', document.location.pathname);
+				this.currentPath = document.location.pathname;
+				this.modifications.forEach(function(m) {
+					if (m.path === this.currentPath) {
+						this.modificationsWaiting.push(m);
+					}
+				});
+			}
+
+			this.modificationsWaiting = this.modificationsWaiting.filter(function(m) {
+				var el = document.querySelector(m.selector);
+				if (!el) {
+					// Keep this modification in the list until its element appears.
+					return true;
+				}
+				m.callback.call(this, el);
+				return false;
+			});
+		},
+
+		currentPath: null,
+
+		modifications: [],
+		modificationsWaiting: [],
+
+		modifyElementForPath: function(path, selector, callback) {
+		},
+	};
+
+	Object.keys(toReturn).forEach(function(k) {
+		if (typeof toReturn[k] === 'function') {
+			toReturn[k] = toReturn[k].bind(toReturn);
 		}
-	},
+	});
 
-	currentPath: null,
+	return toReturn;
+})();
 
-	modifications: [],
-	modificationsWaiting: [],
-
-	modifyElementForPath: function(path, selector, callback) {
-	},
-};
+window.cpForumsEnhancments.modifyElementForPath(
+	'/',
+	'tr[data-category-id=15] > td.category .category-description',
+	function(el) {
+		el.innerHTML = (
+			'<blockquote style="font-weight: bold; margin: 0">'
+			+ el.innerHTML
+			+ '</blockquote>'
+		);
+	}
+);
 
 window.cpForumsEnhancments.init();
